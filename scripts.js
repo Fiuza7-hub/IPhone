81d2d981c573ba2562fafa201d647338640534aa
Fiuza7-hub <127128919+Fiuza7-hub@users.noreply.github.com>
2023-03-19 18:14:04 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+
+
+function trocacor(cor) {let circulo=document.querySelector(".circulo")
+circulo.style.background= cor}
+
+function trocaimagem(imagem){
+  
+  let imgiPhone= document.querySelector(".iPhone") 
+  imgiPhone.src=imagem}
+
+/*let é uma variável*/
+/*para chamar a função se usa o troca-cor() no
+html, no botao*/
